5e453d359aacd14ea5c0f642d4e82882f6ffd47a
20191864229 <851947392@qq.com>
2019-11-08 10:27:56 +0800

    三人都可独立完成此问题。
    Signed-off-by: 20191864229 <851947392@qq.com>

---
+++ 计算天数/11_08.cpp
@@ -0,0 +1,17 @@
+// 11_08.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+
+int main(int argc, char* argv[])
+{
+    int monthday[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+	int year,month,day,sum=0;
+	scanf("%d %d %d",&year,&month,&day);
+	if(year%400==0||(year%4==0&&year%100!=0)) 
+	monthday[1]=29;
+	for(int i=0;i<month-1;i++)
+	sum+=monthday[i];
+	printf("%d\n",sum+day);
+	return 0;
+}
+
